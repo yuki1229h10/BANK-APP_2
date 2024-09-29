34e26af5693b2d0882d7a9c535e4fec96262415c
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-09-29 23:24:40 +0900

    update

--- src/main/java/com/eazybytes/repository/CustomerRepository.java
+++ src/main/java/com/eazybytes/repository/CustomerRepository.java
@@ -1,14 +1,15 @@
 package com.eazybytes.repository;
 
-import com.eazybytes.model.Customer;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.eazybytes.model.Customer;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer,Long> {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
-    Optional<Customer> findByEmail(String email);
+	Optional<Customer> findByEmail(String email);
 
 }
