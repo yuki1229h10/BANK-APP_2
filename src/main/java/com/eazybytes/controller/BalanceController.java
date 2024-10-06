2442bf447ca05cd23b169fe3176dc2345b853a36
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-10-06 22:02:15 +0900

    update

--- src/main/java/com/eazybytes/controller/BalanceController.java
+++ src/main/java/com/eazybytes/controller/BalanceController.java
@@ -1,14 +1,31 @@
 package com.eazybytes.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eazybytes.model.AccountTransactions;
+import com.eazybytes.repository.AccountTransactionsRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class BalanceController {
 
-    @GetMapping("/myBalance")
-    public  String getBalanceDetails () {
-        return "Here are the balance details from the DB";
-    }
+	private final AccountTransactionsRepository accountTransactionsRepository;
+
+	@GetMapping("/myBalance")
+	public List<AccountTransactions> getBalanceDetails(@RequestParam long id) {
+		List<AccountTransactions> accountTransactions = accountTransactionsRepository
+				.findByCustomerIdOrderByTransactionDtDesc(id);
+		if (accountTransactions != null) {
+			return accountTransactions;
+		} else {
+			return null;
+		}
+	}
 
 }
